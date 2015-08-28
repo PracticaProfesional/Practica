bfd7c5ec969b042b370c2fd34803c49c75d0d353
QUINCHO <QUINCHO@10.68.242.173>
2015-08-28 16:22:07 -0600

    Commit de prueba

--- src/datos/DatosPaciente.java
+++ src/datos/DatosPaciente.java
@@ -1,5 +1,24 @@
 package datos;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatosPaciente {
     
-}
+    private Conexion objetoDeConexion;
+    Statement estado;
+    
+    public void insertarPaciente()
+    {
+        try{
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery(null);
+            estado.close();
+        }// fin de try
+        catch (SQLException e)
+        {
+            
+        }// fin de catch
+    }// fin del metodo insertarPaciente
+    
+}// fin de la clase DatosPaciente
