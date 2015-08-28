19e2be9853478764970b70968d26de85730b6369
cooper15 <cooper15@minti2>
2015-08-28 12:46:42 -0600

    Agregado ExpedienteNuevo y tab consulta medica

--- src/presentacion/Inicio.java
+++ src/presentacion/Inicio.java
@@ -39,6 +39,17 @@ public class Inicio extends javax.swing.JFrame {
         panelPrincipal = new javax.swing.JTabbedPane();
         tabAdministrador = new javax.swing.JTabbedPane();
         tabExpediente = new javax.swing.JTabbedPane();
+        tabBuscarPaciente = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tableBuscarPaciente = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        btnCrearExpediente = new javax.swing.JButton();
+        btnConsultaMedica = new javax.swing.JButton();
+        tabConsultaMedica = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         tabInventario = new javax.swing.JTabbedPane();
         tapAgenda = new javax.swing.JTabbedPane();
         tabReportes = new javax.swing.JTabbedPane();
@@ -53,6 +64,127 @@ public class Inicio extends javax.swing.JFrame {
         panelPrincipal.setToolTipText("");
         panelPrincipal.setName("Panel principal"); // NOI18N
         panelPrincipal.addTab("Administrador", tabAdministrador);
+
+        tableBuscarPaciente.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(tableBuscarPaciente);
+
+        jButton1.setText("Buscar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Cédula Paciente");
+
+        btnCrearExpediente.setText("Nuevo");
+        btnCrearExpediente.setEnabled(false);
+        btnCrearExpediente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCrearExpedienteActionPerformed(evt);
+            }
+        });
+
+        btnConsultaMedica.setText("Consulta Medica");
+
+        javax.swing.GroupLayout tabBuscarPacienteLayout = new javax.swing.GroupLayout(tabBuscarPaciente);
+        tabBuscarPaciente.setLayout(tabBuscarPacienteLayout);
+        tabBuscarPacienteLayout.setHorizontalGroup(
+            tabBuscarPacienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+            .addGroup(tabBuscarPacienteLayout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addComponent(jLabel1)
+                .addGap(26, 26, 26)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(jButton1)
+                .addContainerGap(46, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, tabBuscarPacienteLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnConsultaMedica)
+                .addGap(30, 30, 30)
+                .addComponent(btnCrearExpediente, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21))
+        );
+        tabBuscarPacienteLayout.setVerticalGroup(
+            tabBuscarPacienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(tabBuscarPacienteLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(tabBuscarPacienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1)
+                    .addComponent(jLabel1))
+                .addGap(23, 23, 23)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addGroup(tabBuscarPacienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCrearExpediente)
+                    .addComponent(btnConsultaMedica))
+                .addGap(21, 21, 21))
+        );
+
+        tabExpediente.addTab("Buscar Paciente", tabBuscarPaciente);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Signos Vitales"));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 164, Short.MAX_VALUE)
+        );
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Consulta"));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 599, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 137, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout tabConsultaMedicaLayout = new javax.swing.GroupLayout(tabConsultaMedica);
+        tabConsultaMedica.setLayout(tabConsultaMedicaLayout);
+        tabConsultaMedicaLayout.setHorizontalGroup(
+            tabConsultaMedicaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, tabConsultaMedicaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(tabConsultaMedicaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        tabConsultaMedicaLayout.setVerticalGroup(
+            tabConsultaMedicaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(tabConsultaMedicaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+
+        tabExpediente.addTab("Consulta Medica", tabConsultaMedica);
+
         panelPrincipal.addTab("Expediente", tabExpediente);
         panelPrincipal.addTab("Inventario", tabInventario);
         panelPrincipal.addTab("Agenda", tapAgenda);
@@ -72,14 +204,14 @@ public class Inicio extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(panelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 728, Short.MAX_VALUE)
+                .addComponent(panelPrincipal)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(panelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 385, Short.MAX_VALUE)
+                .addComponent(panelPrincipal)
                 .addContainerGap())
         );
 
@@ -87,6 +219,15 @@ public class Inicio extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void btnCrearExpedienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCrearExpedienteActionPerformed
+        ExpedienteNuevo nuevo = new ExpedienteNuevo(this, true);
+        nuevo.setVisible(true);
+    }//GEN-LAST:event_btnCrearExpedienteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,14 +265,25 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnConsultaMedica;
+    private javax.swing.JButton btnCrearExpediente;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTabbedPane panelPrincipal;
     private javax.swing.JTabbedPane tabAdministrador;
+    private javax.swing.JPanel tabBuscarPaciente;
+    private javax.swing.JPanel tabConsultaMedica;
     private javax.swing.JTabbedPane tabExpediente;
     private javax.swing.JTabbedPane tabInventario;
     private javax.swing.JTabbedPane tabReportes;
+    private javax.swing.JTable tableBuscarPaciente;
     private javax.swing.JTabbedPane tapAgenda;
     // End of variables declaration//GEN-END:variables
 }
