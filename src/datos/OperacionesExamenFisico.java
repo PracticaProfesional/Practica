33de6c34a7a6d4778facc4ad540327d7c8a1947b
cooper15 <cooper15@minti2>
2015-09-03 11:41:39 -0600

    Creada clase OperacionesExamenFisico

---
+++ src/datos/OperacionesExamenFisico.java
@@ -0,0 +1,17 @@
+
+package datos;
+
+import java.sql.Statement;
+
+/**
+ *
+ * @author cooper15
+ */
+public class OperacionesExamenFisico {
+    private Conexion objetoDeConexion;
+    private Statement estado;
+    
+    public void insertarExamenFisico(){
+        
+    }
+}
