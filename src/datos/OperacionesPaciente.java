eae3f54cd892ab5f539a533bed00b8abc896cc4f
cooper15 <cooper15@minti2>
2015-09-03 09:01:42 -0600

    renombrada la clase DatosPaciente a OperacionesPaciente

---
+++ src/datos/OperacionesPaciente.java
@@ -0,0 +1,31 @@
+package datos;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import entidad.Paciente;
+
+public class OperacionesPaciente {
+    
+    private Conexion objetoDeConexion;
+    private Statement estado;
+    
+    public void insertarPaciente(Paciente objetoPaciente)
+    { 
+        String datosPaciente = "'"+objetoPaciente.getNombrePaciente()+"'"+","+"'"+objetoPaciente.getApellido1()+"'"+","
+                                +"'"+objetoPaciente.getSexo()+"'"+","+"'"+objetoPaciente.getFechaNacimiento()+"'"+","
+                                +"'"+objetoPaciente.getNacionalidad()+"'"+","+"'"+objetoPaciente.getIdentificacion()+"'"+","
+                                +"'"+objetoPaciente.getTelefono()+"'"+","+"'"+objetoPaciente.getDireccion1()+"'"+","
+                                +"'"+objetoPaciente.getDireccion2()+"'"+","+"'"+objetoPaciente.getEmail()+"'";
+        try
+        {
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("Call ("+datosPaciente+")");
+            estado.close();
+        }// fin de try
+        catch (SQLException e)
+        {
+            
+        }// fin de catch
+    }// fin del metodo insertarPaciente
+    
+}// fin de la clase OperacionesPaciente
