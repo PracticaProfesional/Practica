6cce368dfd23a3d9f11cbe046c28f87a67f65454
cooper15 <cooper15@minti2>
2015-09-03 08:56:52 -0600

    Creada operacion insertarPaciente en la capa de datos

--- src/datos/DatosPaciente.java
+++ src/datos/DatosPaciente.java
@@ -10,11 +10,16 @@ public class DatosPaciente {
     private Statement estado;
     
     public void insertarPaciente(Paciente objetoPaciente)
-    {
+    { 
+        String datosPaciente = "'"+objetoPaciente.getNombrePaciente()+"'"+","+"'"+objetoPaciente.getApellido1()+"'"+","
+                                +"'"+objetoPaciente.getSexo()+"'"+","+"'"+objetoPaciente.getFechaNacimiento()+"'"+","
+                                +"'"+objetoPaciente.getNacionalidad()+"'"+","+"'"+objetoPaciente.getIdentificacion()+"'"+","
+                                +"'"+objetoPaciente.getTelefono()+"'"+","+"'"+objetoPaciente.getDireccion1()+"'"+","
+                                +"'"+objetoPaciente.getDireccion2()+"'"+","+"'"+objetoPaciente.getEmail()+"'";
         try
         {
             estado = objetoDeConexion.conectar().createStatement();
-            estado.executeQuery(null);
+            estado.executeQuery("Call ("+datosPaciente+")");
             estado.close();
         }// fin de try
         catch (SQLException e)
