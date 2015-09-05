e3138186bad1316c39079d70c3026997763cab9d
cooper15 <cooper15@minti2>
2015-09-04 21:41:08 -0600

    Creado metodo para insertar en la tabala antecedentes personales

---
+++ src/datos/OperacionesAntecedentesPersonales.java
@@ -0,0 +1,30 @@
+package datos;
+
+import entidad.AntecedentesPersonales; // cambiar al paquete de negocios.
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author cooper15
+ */
+public class OperacionesAntecedentesPersonales {
+    private Conexion objetoDeConexion;
+    private Statement estado;
+    
+    public void insertarAntecedentesPersonales(AntecedentesPersonales antecPersonales) throws SQLException{
+        String idVacuna;
+        String idPadecimiento;
+        String idAlergia;
+        ObtenerUltimoId obtenerId = new ObtenerUltimoId();
+        idVacuna = obtenerId.obtenerUltimoId("Vacunas");
+        idPadecimiento = obtenerId.obtenerUltimoId("Padecimientos");
+        idAlergia = obtenerId.obtenerUltimoId("Alergias");
+        String datosAntecPersonales = "'"+idAlergia+"'"+","+"'"+antecPersonales.getTratamiento()+"'"
+                                      +","+"'"+idPadecimiento+"'"+","+"'"+idVacuna+"'"+","
+                                      +"'"+antecPersonales.getMedicamento()+"'";
+        estado = objetoDeConexion.conectar().createStatement();
+        estado.executeQuery("Call InsertarAntecedentesPersonales("+datosAntecPersonales+")");
+        estado.close();
+    }
+}
