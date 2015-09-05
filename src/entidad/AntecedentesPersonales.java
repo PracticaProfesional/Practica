627578ca3027b9332549a8a39d9bd52503e6a216
cooper15 <cooper15@minti2>
2015-09-04 21:40:40 -0600

    Eliminados atributos innecesarios

--- src/entidad/AntecedentesPersonales.java
+++ src/entidad/AntecedentesPersonales.java
@@ -10,35 +10,9 @@ package entidad;
  * @author cooper15
  */
 public class AntecedentesPersonales {
-    private int alergia;
-    private int padecimiento;
-    private int vacuna;
+ 
     private String tratamiento;
     private String medicamento;
-
-    public int getAlergia() {
-        return alergia;
-    }
-
-    public void setAlergia(int alergia) {
-        this.alergia = alergia;
-    }
-
-    public int getPadecimiento() {
-        return padecimiento;
-    }
-
-    public void setPadecimiento(int padecimiento) {
-        this.padecimiento = padecimiento;
-    }
-
-    public int getVacuna() {
-        return vacuna;
-    }
-
-    public void setVacuna(int vacuna) {
-        this.vacuna = vacuna;
-    }
 
     public String getTratamiento() {
         return tratamiento;
