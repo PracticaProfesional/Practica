23674803feb8d82896dcdb37e2774aa4f1c78610
cooper15 <cooper15@minti2>
2015-09-07 10:23:47 -0600

    Creacion clase antecedente familiar capa datos

---
+++ src/datos/OperacionesAntecedentesFamiliares.java
@@ -0,0 +1,21 @@
+package datos;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import entidad.AntecedenteFamiliar;
+/**
+ *
+ * @author cooper15
+ */
+public class OperacionesAntecedentesFamiliares {
+    private Conexion objetoDeConexion;
+    private Statement estado;
+    public void insertarAntecedentesFamiliares(AntecedenteFamiliar antecedenteFamiliar) throws SQLException{
+        String idPadecimientos;
+        ObtenerUltimoId ultimoId = new ObtenerUltimoId();
+        idPadecimientos = ultimoId.obtenerUltimoId("Padecimientos");
+        estado = objetoDeConexion.conectar().createStatement();
+        String datosAntecedentesFamiliares = "'"+idPadecimientos+"'"+","+"'"+antecedenteFamiliar.getParentezco()+"'";
+        estado.executeQuery("Call InsertarAntecedentesFamiliares ("+datosAntecedentesFamiliares+")");
+    }    
+}
