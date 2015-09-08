f062764a05c1563ea29f1e13157edd821a26169c
cooper15 <cooper15@minti2>
2015-09-07 21:54:54 -0600

    Revert commit cambios project.properties

---
+++ src/entidad/Vacunas.java
@@ -0,0 +1,27 @@
+package entidad;
+
+/**
+ *
+ * @author cooper15
+ */
+public class Vacunas {
+    private String tipo;
+    private String fechaAplicacion;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFechaAplicacion() {
+        return fechaAplicacion;
+    }
+
+    public void setFechaAplicacion(String fechaAplicacion) {
+        this.fechaAplicacion = fechaAplicacion;
+    }
+    
+}
