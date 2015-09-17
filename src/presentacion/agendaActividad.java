57526f8c01964e8ff2333bfdcf158e4e2767e465
cooper15 <v.centeno15@gmail.com>
2015-09-17 10:09:48 -0600

    Se agrego un metodo trim para evitar que la cadena tenga espacios en blanco en los extremos.

--- src/presentacion/agendaActividad.java
+++ src/presentacion/agendaActividad.java
@@ -110,7 +110,7 @@ public class agendaActividad extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        detalles = textDetalles.getText();
+        detalles = textDetalles.getText().trim();
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
