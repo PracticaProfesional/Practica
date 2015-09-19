cb472d9c20803fe3be73005be59f9bc2bc114dd2
cooper15 <v.centeno15@gmail.com>
2015-09-19 15:34:51 -0600

    Metodo para obtener los datos de examen fisico

--- src/datos/OperacionesExamenFisico.java
+++ src/datos/OperacionesExamenFisico.java
@@ -4,6 +4,9 @@ package datos;
 import java.sql.Statement;
 import entidad.ExamenFisico;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class OperacionesExamenFisico 
 {
@@ -19,8 +22,20 @@ public class OperacionesExamenFisico
                                   "'" + objetoExamenFisico.getDetalle() + "'";
        
         estado = objetoDeConexion.conectar().createStatement();
-        estado.executeQuery("Call InsertarConsultaMedica("+datosExamenFisico+")");
+        estado.executeQuery("Call InsertarExamenFisico("+datosExamenFisico+")");
         estado.close();
     }// fin del metodo insertarExamenFisico
+    
+    public ResultSet obtenerExamenFisico(String id){
+       objetoDeConexion = new Conexion();     
+       ResultSet rs = null;
+        try {
+            estado = objetoDeConexion.conectar().createStatement();
+            rs = estado.executeQuery("call ObtenerExamenFisico("+id+")");
+        } catch (SQLException ex) {
+            Logger.getLogger(OperacionesExamenFisico.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
     
 }// fin de la clase OperacionesExamenFisico
