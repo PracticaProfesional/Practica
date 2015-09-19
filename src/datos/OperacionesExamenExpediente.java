a49e5387c152267b67d7fa8bbe21f2f07b67eda7
cooper15 <v.centeno15@gmail.com>
2015-09-19 13:34:18 -0600

    Metodos de insertar  y consultar: el de consulta obtiene los id de examen fisico presentes en la tabla relacion examen-expediente

--- src/datos/OperacionesExamenExpediente.java
+++ src/datos/OperacionesExamenExpediente.java
@@ -1,9 +1,36 @@
 package datos;
 
+import java.sql.Statement;
+
 /**
  *
  * @author cooper15
  */
 public class OperacionesExamenExpediente {
-    
+    private Conexion objetoDeConexion;
+    private Statement estado;
+    public void insertarRelacionExamenExpediente(String idExFis, String idExp){
+        objetoDeConexion = new Conexion();
+        try{
+            String datos = "'"+idExFis+"'"+","+"'"+idExp+"'";
+            estado  = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("call InsertarExFisicoExpediente("+datos+")");
+        }
+        catch(java.sql.SQLException e){
+        }
+        
+    }
+    // Obtiene los id de padecimientos en examen fisico que estan relacionados con un expediente.
+    public java.sql.ResultSet obtenerIdExamenesMedicos(String idExpediente){
+        objetoDeConexion = new Conexion();
+        java.sql.ResultSet rs = null;
+        try{
+            estado = objetoDeConexion.conectar().createStatement();
+            rs = estado.executeQuery("Call obtenerIdExamenExpediente("+idExpediente+")");
+        }
+        catch(java.sql.SQLException E){
+            
+        }
+        return rs;
+    }
 }
