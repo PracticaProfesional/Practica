5459ae428b55d99e111e2a95a6d41bb646989d8a
cooper15 <v.centeno15@gmail.com>
2015-09-22 15:32:50 -0600

    Cambios en los atributos de la tabla

--- src/entidad/ConsultaMedica.java
+++ src/entidad/ConsultaMedica.java
@@ -6,10 +6,36 @@ package entidad;
  * @author cooper15
  */
 public class ConsultaMedica {
-    private String motivoConsulta;
-    private String sintomaPaciente;
-    private String diagnostico;
-    private int expediente;
+    private String idReceta = "";
+    private String fecha = "";
+    private String motivoConsulta = "";
+    private String sintomaPaciente = "";
+    private String diagnostico = "";
+    private int idexpediente = 0;
+
+    public String getIdReceta() {
+        return idReceta;
+    }
+
+    public void setIdReceta(String idReceta) {
+        this.idReceta = idReceta;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getIdexpediente() {
+        return idexpediente;
+    }
+
+    public void setIdexpediente(int idexpediente) {
+        this.idexpediente = idexpediente;
+    }
 
     public String getMotivoConsulta() {
         return motivoConsulta;
