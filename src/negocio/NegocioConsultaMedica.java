32df0339dbbaf2d131602aa9d45d069f5d5e82c6
cooper15 <v.centeno15@gmail.com>
2015-09-22 20:52:37 -0600

    Se elimino el try innecesario

--- src/negocio/NegocioConsultaMedica.java
+++ src/negocio/NegocioConsultaMedica.java
@@ -4,20 +4,16 @@ package negocio;
 
 import entidad.ConsultaMedica;
 import datos.OperacionesConsultaMedica;
-import java.sql.SQLException;
 
 public class NegocioConsultaMedica 
 {
     public void insertarConsultaMedica(ConsultaMedica nuevaConsultaMedica)
     {
-        try
-        {
+        
             OperacionesConsultaMedica operacionesConsultaMedica = new OperacionesConsultaMedica();
             operacionesConsultaMedica.insertarConsultaMedica(nuevaConsultaMedica);
-        }// fin de try
-        catch(SQLException e)
-        {
-        }// fin de catch
+        
+      
     
     }// fin del metodo insertarConsultaMedica
     
