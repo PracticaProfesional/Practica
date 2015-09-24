2137fcef58eb9be3467392da47cdf20544109ab1
cooper15 <v.centeno15@gmail.com>
2015-09-24 15:10:42 -0600

    Se agrego nuevo metodo para actualizar la tabla de consulta medica

--- src/negocio/NegocioConsultaMedica.java
+++ src/negocio/NegocioConsultaMedica.java
@@ -9,9 +9,11 @@ public class NegocioConsultaMedica
 {
     public void insertarConsultaMedica(ConsultaMedica nuevaConsultaMedica)
     {
-        
-            OperacionesConsultaMedica operacionesConsultaMedica = new OperacionesConsultaMedica();
-            operacionesConsultaMedica.insertarConsultaMedica(nuevaConsultaMedica);
+        OperacionesConsultaMedica operacionesConsultaMedica = new OperacionesConsultaMedica();
+        operacionesConsultaMedica.insertarConsultaMedica(nuevaConsultaMedica);
     }// fin del metodo insertarConsultaMedica
-    
+    public void actualizarConsultaMedica(String idExpediente, entidad.ConsultaMedica actConsulta){
+        OperacionesConsultaMedica operacionesConsultaMedica = new OperacionesConsultaMedica();
+        operacionesConsultaMedica.actualizarConsultaMedica(idExpediente, actConsulta);
+    }
 }// fin de la clase NegocioConsultaMedica
