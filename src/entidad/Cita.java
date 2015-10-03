c07628cc7930df95f31ab96c59376dda7a629ce4
QUINCHO <QUINCHO@QUINCHO-THINK>
2015-10-03 17:49:11 -0600

    Incluido en la clase Cita el atributo de tipo enumerado

--- src/entidad/Cita.java
+++ src/entidad/Cita.java
@@ -6,12 +6,20 @@ package entidad;
  */
 public class Cita {
     //private int detalle;
-    private int estado;
+    private EstadoCitaEnum estado;
     private String fechaCreacion;
     private String fechaConsulta;
     private String anotaciones;
     private String hora;
 
+    
+    public EstadoCitaEnum getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCitaEnum estado) {
+        this.estado = estado;
+    }
     public String getHora() {
         return hora;
     }
@@ -20,13 +28,6 @@ public class Cita {
         this.hora = hora;
     }
 
-    public int getEstado() {
-        return estado;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
 
     public String getFechaCreacion() {
         return fechaCreacion;
