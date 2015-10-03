04e6361a1609c8c7bd28327d1b936a9b23b5906d
cooper15 <v.centeno15@gmail.com>
2015-10-02 21:32:19 -0600

    Cambio de ruta de icono

--- src/presentacion/AgendaActividad.java
+++ src/presentacion/AgendaActividad.java
@@ -61,7 +61,7 @@ public class AgendaActividad extends javax.swing.JDialog {
             }
         });
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("/home/cooper15/NetBeansProjects/SIGOS/Practica/img/compose.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/presentacion/compose.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
