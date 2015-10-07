a30ee3cc2d30ec763f5a6c41868919e906a94888
cooper15 <v.centeno15@gmail.com>
2015-10-06 21:49:12 -0600

    Enum EstadoCita cambiado a sub capa enums

---
+++ src/entidad/enums/EstadoCitaEnum.java
@@ -0,0 +1,23 @@
+// @author QUINCHO
+
+package entidad.enums;
+ 
+public enum EstadoCitaEnum 
+{
+    Pendiente (1),    // 1
+    Completada (2),   // 2
+    Cancelada (3);    // 3
+    
+    private int numero;
+    
+    private EstadoCitaEnum(int numero)
+    {
+        this.numero = numero;
+    }// fin del constructor de EstadoCita
+    
+    public int obtenerEstadoNumero()
+    {
+        return numero;
+    }// fin del metodo obtenerEstadoNumero
+    
+}// fin de la clase EstadoCitaEnum
