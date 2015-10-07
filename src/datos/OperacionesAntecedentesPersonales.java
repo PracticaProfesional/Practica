c274b723a7888b23fb33ee5f55176dbcce7a1858
cooper15 <v.centeno15@gmail.com>
2015-10-06 21:39:13 -0600

    Se cambio la forma en la que se manejan las excepciones

--- src/datos/OperacionesAntecedentesPersonales.java
+++ src/datos/OperacionesAntecedentesPersonales.java
@@ -12,22 +12,28 @@ public class OperacionesAntecedentesPersonales {
     private Conexion objetoDeConexion;
     private Statement estado;
     
-    public void insertarAntecedentesPersonales(AntecedentesPersonales antecPersonales) throws SQLException{
+    public void insertarAntecedentesPersonales(AntecedentesPersonales antecPersonales){
         objetoDeConexion = new Conexion();
         String idVacuna;
         String idPadecimiento;
         String idAlergia;
         ObtenerUltimoId obtenerId = new ObtenerUltimoId();
-        idVacuna = obtenerId.obtenerUltimoId("Vacunas");
-        idPadecimiento = obtenerId.obtenerUltimoId("Padecimientos");
-        idAlergia = obtenerId.obtenerUltimoId("Alergias");
+        try{
+            idVacuna = obtenerId.obtenerUltimoId("Vacunas");
+            idPadecimiento = obtenerId.obtenerUltimoId("Padecimientos");
+            idAlergia = obtenerId.obtenerUltimoId("Alergias");
+
+            String datosAntecPersonales = "'" + antecPersonales.getTratamiento() + "'"+ "," + 
+                                          "'" + antecPersonales.getMedicamento() + "'" + "," +
+                                          "'" + antecPersonales.getAlergias() + "'" + "," +
+                                          "'" + antecPersonales.getVacunas() + "'";
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("Call InsertarAntecedentesPersonales("+datosAntecPersonales+")");
+            estado.close();
+        }
+        catch(java.sql.SQLException e){
+            System.out.println(e.getErrorCode() + e.getMessage());
+        }
         
-        String datosAntecPersonales = "'" + antecPersonales.getTratamiento() + "'"+ "," + 
-                                      "'" + antecPersonales.getMedicamento() + "'" + "," +
-                                      "'" + antecPersonales.getAlergias() + "'" + "," +
-                                      "'" + antecPersonales.getVacunas() + "'";
-        estado = objetoDeConexion.conectar().createStatement();
-        estado.executeQuery("Call InsertarAntecedentesPersonales("+datosAntecPersonales+")");
-        estado.close();
     }
 }
