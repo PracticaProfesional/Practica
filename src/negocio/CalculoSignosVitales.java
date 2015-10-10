c232760b195182be0c2e2cf3ab8044b3562713e9
cooper15 <v.centeno15@gmail.com>
2015-10-10 11:52:45 -0600

    Metodos para calcular el IMC y el IMB

---
+++ src/negocio/CalculoSignosVitales.java
@@ -0,0 +1,33 @@
+package negocio;
+
+/**
+ *
+ * @author cooper15
+ */
+public class CalculoSignosVitales {
+
+    /**
+     * El IMC es una medida de relacion entre la masa y la talla.
+     * La talla ha de ser expresada en centimetros.
+     * @param peso 
+     * @param talla
+     * @return imc
+     */
+    public double getIndiceMasaCorporal(double peso, double talla){
+        double imc;
+        imc = peso / Math.pow(talla, 2);
+        return imc;
+    }
+    
+    public double getIndiceMetabolicoBasal(entidad.enums.SexoEnum sexo, 
+                                           double peso, double talla, int edad){
+        double imb = 0.0;
+        switch(sexo.getSexo()){
+            case 1: imb = 66 + (13.75 * peso) + (5.08 * talla) - (6.78 * edad);
+                break;
+            case 2: imb = 655 + (9.56 * peso) + (1.85 * talla) - (4.68 * edad);
+                break;
+        }
+        return imb;
+    }
+}
