e16156fe45b5b37ecadff8bc96abfccd8032b8c3
QUINCHO <QUINCHO@QUINCHO-THINK>
2015-10-30 17:45:34 -0600

    Agregado el metodo obtenerReceta (para verificar que respetaba los saltos de linea)

--- src/negocio/NegocioReceta.java
+++ src/negocio/NegocioReceta.java
@@ -2,21 +2,21 @@ package negocio;
 
 import entidad.Receta;
 import datos.OperacionesReceta;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class NegocioReceta 
 {
-    public void insertarCita(Receta nuevaReceta)
+    public void insertarReceta(Receta nuevaReceta)
     {
-        try
-        {
-            OperacionesReceta operacionesReceta = new OperacionesReceta();
-            operacionesReceta.insertarReceta(nuevaReceta);
-        }// fin de try
-        catch(SQLException e)
-        {
-        }// fin de catch
-    
+        OperacionesReceta operacionesReceta = new OperacionesReceta();
+        operacionesReceta.insertarReceta(nuevaReceta);    
     }// fin del metodo insertarReceta
+    
+    public ResultSet obtenerReceta(String id) throws SQLException
+    {
+        OperacionesReceta operacionesReceta = new OperacionesReceta();
+        return operacionesReceta.obtenerReceta(id);
+    }// fin del metodo obtenerReceta
     
 }// fin de la clase NegocioReceta
