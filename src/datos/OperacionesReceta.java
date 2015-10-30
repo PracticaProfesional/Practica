be1df19196037d317cf1898a7d36a00d9a0f5e57
QUINCHO <QUINCHO@QUINCHO-THINK>
2015-10-30 17:45:15 -0600

    Agregado el metodo obtenerReceta (para verificar que respetaba los saltos de linea)

--- src/datos/OperacionesReceta.java
+++ src/datos/OperacionesReceta.java
@@ -2,6 +2,7 @@
 package datos;
 
 import entidad.Receta;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,16 +12,32 @@ public class OperacionesReceta
     private Statement estado;
     
     
-    public void insertarReceta(Receta objetoReceta) throws SQLException
+    public void insertarReceta(Receta nuevaReceta)
     {
         objetoDeConexion = new Conexion();
        
-        String datosCita = "'" + objetoReceta.getNombreMedicamento() + "'" + "," + 
-                             "'" + objetoReceta.getCantidad() + "'";
-       
-        estado = objetoDeConexion.conectar().createStatement();
-        estado.executeQuery("Call InsertarCita("+datosCita+")");
-        estado.close();
+        String datosReceta = "'" + nuevaReceta.getDescripcion() + "'";
+        
+        try
+        {
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("Call InsertarReceta("+datosReceta+")");
+            estado.close();
+        }// fin del try
+        catch(SQLException sqle)
+        {
+            System.out.println(sqle.getErrorCode() + sqle.getMessage());
+        }// fin del catch
     }// fin del metodo insertarReceta
+    
+    public ResultSet obtenerReceta(String id) throws SQLException
+    {
+        ResultSet rs;
+        objetoDeConexion = new Conexion();
+        
+        estado = objetoDeConexion.conectar().createStatement();
+        rs = estado.executeQuery("SELECT descripcion FROM Receta WHERE id = " + id);
+        return rs;
+    }// findel metodo obtenerFechaConsulta
     
 }// fin de la clase OperacionesReceta
