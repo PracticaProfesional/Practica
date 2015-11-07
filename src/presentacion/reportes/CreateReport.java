480350d095f7f57e4de1b58e77ce55115cc0c666
cooper15 <v.centeno15@gmail.com>
2015-11-07 14:49:49 -0600

    Clase unica para generar los reportes, como parametros se le pasan 1. Parametros para la consulta. 2. ruta del archivo .Jasper

---
+++ src/presentacion/reportes/CreateReport.java
@@ -0,0 +1,46 @@
+package presentacion.reportes;
+
+import java.awt.HeadlessException;
+import java.util.Map;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import negocio.NegocioConexion;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JRViewer;
+
+/**
+ *
+ * @author cooper15
+ */
+public class CreateReport {
+    JasperReport reporte;
+    JasperPrint reporteFill;
+    JRViewer panelVisor;
+    NegocioConexion  nConexion;
+    
+    public void generarReporte(Map params, String ruta){
+       try{
+            nConexion = new NegocioConexion();
+            reporte = (JasperReport) JRLoader.loadObjectFromFile(ruta);
+            reporteFill = JasperFillManager.fillReport(reporte, params,nConexion.conexion());
+           
+            visualizarReporteGenerado();
+       }
+       catch(JRException ex){
+           System.err.println(ex.getMessage());
+       }
+    }
+
+    private void visualizarReporteGenerado() throws HeadlessException {
+        JDialog viewer = new JDialog(new JFrame(),"Vista previa del reporte", true);
+        viewer.setSize(800,600);
+        viewer.setLocationRelativeTo(null);
+        JRViewer jrv = new JRViewer(reporteFill);
+        viewer.getContentPane().add(jrv);
+        viewer.setVisible(true);
+    }
+}
