51163a8df0e63e5943d52e59395ed919681a334f
cooper15 <v.centeno15@gmail.com>
2015-11-23 15:21:06 -0600

    Modificicacion de los metodos para que trabaje con claves cifradas

--- src/datos/OperacionesUsuario.java
+++ src/datos/OperacionesUsuario.java
@@ -5,27 +5,33 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import entidad.Usuario;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 
 public class OperacionesUsuario {
     
     private Conexion objetoDeConexion;
     private Statement estado;
     
-    public entidad.Usuario ObtenerIdUsuario(Usuario Usuario) throws SQLException
+    public entidad.Usuario ObtenerIdUsuario(Usuario usuario) throws SQLException
     { 
         objetoDeConexion = new Conexion();
-        String datosUsuario = "'"+Usuario.getNombreUsuario()+"'"+","+"'"+Usuario.getContrasena()+"'";
+        String datosUsuario = "'"+usuario.getNombreUsuario()+"'"+","+"'"+usuario.getContrasena()+"'";
         ResultSet result = null;
         try
         {
-           estado = objetoDeConexion.conectar().createStatement();
-           result = estado.executeQuery("Call ObtenerIdUsuario("+datosUsuario+")"); 
-            if(result.next()){
-            entidad.Usuario usu = new Usuario();
-            usu.setNombreUsuario(result.getString("nombreUsuario"));
-            usu.setContrasena(result.getString("contrasena"));
-            return usu;
-         }
+            estado = objetoDeConexion.conectar().createStatement();
+            result = estado.executeQuery("Call ObtenerIdUsuario()");
+            negocio.Cifrado decrypt = new negocio.Cifrado();
+            while(result.next()){
+                if(usuario.getNombreUsuario().equals(result.getString("nombreUsuario")) 
+                && decrypt.coincidePassword(usuario.getContrasena(), result.getString("contrasena"))){
+                    usuario.setNombreUsuario(result.getString("nombreUsuario"));
+                    usuario.setContrasena(result.getString("contrasena"));
+                    usuario.setTipoUsuario(Integer.parseInt(result.getString("tipo")));
+                    return usuario;
+                }
+            }
+         
         }// fin de try
         catch (SQLException e)
         {
@@ -34,5 +40,40 @@ public class OperacionesUsuario {
        
         return null;
     }// fin del metodo insertarPaciente
+    
+    public LinkedList listaUsuarios(){
+        entidad.Usuario nUsuario = new entidad.Usuario();
+        LinkedList<entidad.Usuario> listUsuario = new LinkedList<>();
+        objetoDeConexion = new Conexion();
+        ResultSet rs;
+        try{
+            estado = objetoDeConexion.conectar().createStatement();
+            rs = estado.executeQuery("Call ObtenerIdUsuario()");
+            while(rs.next()){
+                nUsuario.setNombreUsuario(rs.getString("nombreUsuario"));
+                nUsuario.setContrasena(rs.getString("contrasena"));
+                nUsuario.setTipoUsuario(Integer.parseInt(rs.getString("tipo")));
+                listUsuario.add(nUsuario);
+            }
+        }
+        catch(SQLException sqle){
+            System.out.println(sqle.getErrorCode() + sqle.getMessage());
+        }  
+       return listUsuario; 
+    }
+    
+    public void insertarUsuario(Usuario nUsuario){
+        objetoDeConexion = new Conexion();
+        String datosUsuario = "'"+nUsuario.getNombreUsuario()+"'"
+                              +","+"'"+nUsuario.getContrasena()+"'"
+                              +","+"'"+nUsuario.getTipoUsuario()+"'";
+        try{
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("Call InsertarUsuario("+datosUsuario+")");
+        }
+        catch(SQLException sqle){
+        
+        }
+    }
     
 }
