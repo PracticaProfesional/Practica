b5a4ae949824490375ac76a9b93d5052969732f0
cooper15 <v.centeno15@gmail.com>
2015-11-30 14:20:11 -0600

    Recupera los datos que existen en el fichero de texto que guarda las configuraciones.

--- src/negocio/LeerEscribirDatosConexion.java
+++ src/negocio/LeerEscribirDatosConexion.java
@@ -1,8 +1,12 @@
 package negocio;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -14,6 +18,10 @@ public class LeerEscribirDatosConexion {
     
     FileWriter fichero = null;
     PrintWriter printer = null;
+    
+    File archivo = null;
+    FileReader lector = null;
+    BufferedReader bLector = null;
         
     public LeerEscribirDatosConexion(Map<String, String> datos){
        this.datos = datos;
@@ -41,5 +49,48 @@ public class LeerEscribirDatosConexion {
     }
     public void guardar(){
         crearArchivo();
+    }
+    
+    private Map<String, String> obtieneDatosCon(){
+        Map<String, String> datosRec = new HashMap<>();
+        
+        try{
+           archivo = new File("src/configuracion/conf.txt"); 
+           lector = new FileReader(archivo);
+           bLector = new BufferedReader(lector);
+           String linea;
+           int contador = 0;
+           while((linea = bLector.readLine()) != null){
+               switch(contador){
+                   case 0: 
+                       datosRec.put("ip", linea);
+                       break;
+                   case 1: 
+                       datosRec.put("user", linea);
+                       break;
+                   case 2: 
+                       datosRec.put("pass", linea);
+                       break;
+               }
+               contador ++;
+           }
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        finally{
+            try{
+                if(null != lector)
+                    lector.close();
+            }
+            catch(Exception e){
+                System.err.println(e.getMessage());
+            }
+        }
+        return datosRec;
+    }
+    
+    public Map<String, String> recuperarDatos(){
+        return obtieneDatosCon();
     }
 }
