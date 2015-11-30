cfce6b45de5d4c6675b6985f82a9ea018409b27f
cooper15 <v.centeno15@gmail.com>
2015-11-30 13:51:48 -0600

    se le cambio el nombre a la clase ga

---
+++ src/negocio/LeerEscribirDatosConexion.java
@@ -0,0 +1,45 @@
+package negocio;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+/**
+ *
+ * @author cooper15
+ */
+public class LeerEscribirDatosConexion {
+    Map<String, String> datos;
+    
+    FileWriter fichero = null;
+    PrintWriter printer = null;
+        
+    public LeerEscribirDatosConexion(Map<String, String> datos){
+       this.datos = datos;
+    }
+    
+    private void crearArchivo(){
+        try{
+            fichero = new FileWriter("src/configuracion/conf.txt");
+            printer = new PrintWriter(fichero);
+            printer.println(datos.get("ip"));
+            printer.println(datos.get("user"));
+            printer.println(datos.get("pass"));
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        finally{
+            if(null != fichero)
+                try {
+                    fichero.close();
+            } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+            }
+        }
+    }
+    public void guardar(){
+        crearArchivo();
+    }
+}
