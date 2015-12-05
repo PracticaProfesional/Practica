14c9f97d9ec5cb20772ce63c643a8609752a774a
QUINCHO <QUINCHO@QUINCHO-THINK>
2015-12-05 12:23:31 -0600

    Agregados todos los comentarios para javadoc en el sub paquete de enumerados

--- src/entidad/enums/TipoEnum.java
+++ src/entidad/enums/TipoEnum.java
@@ -1,12 +1,29 @@
-// @author QUINCHO
+
  
 package entidad.enums;
 
+/**
+ * Este enumerado representa el tipo de un Paciente; Administrativo, Estudiante, Docente
+ * @author QUINCHO
+ * @version 1.0, 05/12/2015
+ */
 public enum TipoEnum
 {
-    ADMINISTRATIVO (1),   // 1
-    ESTUDIANTE (2),       // 2
-    DOCENTE (3);          // 3
+
+    /**
+     * Representa el tipo Administrativo, valor 1
+     */
+        ADMINISTRATIVO (1),   // 1
+
+    /**
+     * Representa el tipo Estudiante, valor 2
+     */
+        ESTUDIANTE (2),       // 2
+
+    /**
+     * Representa el tipo Docente, valor 3
+     */
+        DOCENTE (3);          // 3
     
     private final int numero;
     
@@ -15,6 +32,10 @@ public enum TipoEnum
         this.numero = numero;
     }// fin del constructor de TipoEnum
     
+    /**
+     * Obitene el numero que representa el tipo del Paciente
+     * @return un entero que representa el tipo del Paciente; Administrativo, Estudiante, Docente
+     */
     public int obtenerTipoNumero()
     {
         return numero;
