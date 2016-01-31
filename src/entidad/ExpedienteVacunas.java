707412e2e867e8040804cf8a12a2d4bcc9575615
QUINCHO <QUINCHO@QUINCHO-THINK>
2016-01-30 18:06:27 -0600

    Agregada clase que mapea la tabla Expediente-Vacunas de la base de datos

---
+++ src/entidad/ExpedienteVacunas.java
@@ -0,0 +1,43 @@
+package entidad;
+
+/**
+ *
+ * @author QUINCHO
+ */
+public class ExpedienteVacunas 
+{
+    private int idVacuna;
+    private int idExpediente;
+    private String fecha;
+
+    public int getIdVacuna()
+    {
+        return idVacuna;
+    }// fin del metodo getIdVacuna
+
+    public void setIdVacuna(int idVacuna) 
+    {
+        this.idVacuna = idVacuna;
+    }// fin del metodo setIdVacuna
+
+    public int getIdExpediente() 
+    {
+        return idExpediente;
+    }// fin del metodo getIdExpediente
+
+    public void setIdExpediente(int idExpediente) 
+    {
+        this.idExpediente = idExpediente;
+    }// fin del metodo setIdExpediente
+
+    public String getFecha() 
+    {
+        return fecha;
+    }// fin del metodo getFecha
+
+    public void setFecha(String fecha) 
+    {
+        this.fecha = fecha;
+    }// fin del metodo setFecha
+    
+}// fin de la clase ExpedienteVacunas
