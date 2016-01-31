b2d2d9e9917d01b5cb6bbc8c8f0319db2f1b3b06
QUINCHO <QUINCHO@QUINCHO-THINK>
2016-01-30 18:12:38 -0600

    Agregado el metodo consultarInsertarVacuna y obtenerIdVacuna

--- src/datos/OperacionesVacuna.java
+++ src/datos/OperacionesVacuna.java
@@ -3,6 +3,7 @@ package datos;
 import entidad.Vacuna;
 import java.sql.Statement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 /**
  * Esta clase realiza las operaciones correspondientes a Vacuna en la base de datos
  * @author cooper15
@@ -24,7 +25,48 @@ public class OperacionesVacuna {
             estado.close();
         }
         catch(SQLException e){
-        
+            System.out.println(e.getErrorCode() + e.getMessage());
         }
-    }
-}
+    }// fin del metodo insertarVacuna
+    
+    /**
+     * Inserta un registros de vacunas en la base de datos, consultando si esta o no insertada anteriormente
+     * @param nombreVacuna de tipo vacuna que contiene los datos a insertar
+     */
+    public void consultarInsertarVacuna(String nombreVacuna)
+    {
+        objetoDeConexion = new Conexion();
+        
+        try
+        {
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("Call ConsultarInsertarVacuna('" + nombreVacuna + "')");
+            estado.close();
+        }// fin del try
+        catch (SQLException e)
+        {
+            System.out.println(e.getErrorCode() + e.getMessage());
+        }// fin de catch
+    }// fin del metodo ConsultarInsertarVacuna
+    
+    public String obtenerIdVacuna(String nombreVacuna)
+    {
+        String id = "";
+        objetoDeConexion = new Conexion();
+        
+        try
+        {
+            estado = objetoDeConexion.conectar().createStatement();
+            ResultSet rs = estado.executeQuery("Call ObtenerIdVacuna ('" + nombreVacuna + "')");
+            
+            if(rs.next())
+                id = rs.getString("id");       
+        }// fin del try
+        catch(java.sql.SQLException sqle)
+        {
+            System.err.println(sqle.getErrorCode() + sqle.getMessage());
+        }//fin del catch
+        
+        return id;
+    }// fin del metodo obtenerId
+}// fin de la clase OperacionesVacuna
