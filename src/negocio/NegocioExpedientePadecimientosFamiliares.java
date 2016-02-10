01a592a91064ef27731789e3c67ae73b40b9f96e
QUINCHO <QUINCHO@QUINCHO-THINK>
2016-02-09 19:10:11 -0600

    agregado metodo para eliminar

--- src/negocio/NegocioExpedientePadecimientosFamiliares.java
+++ src/negocio/NegocioExpedientePadecimientosFamiliares.java
@@ -2,6 +2,8 @@ package negocio;
 
 import datos.OperacionesExpedientePadecimientosFamiliares;
 import entidad.ExpedientePadecimientosFamiliares;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -16,4 +18,19 @@ public class NegocioExpedientePadecimientosFamiliares
                 new OperacionesExpedientePadecimientosFamiliares();
         objOperExpPade.insertarExpedientePadecimientosFamiliares(objExpPadFam);
     }// fin del metodo insertarExpedientePadecimientos
+    
+    public ResultSet listarPadecimientosFamiliares(String idExpedientePaciente) throws SQLException
+    {
+        OperacionesExpedientePadecimientosFamiliares objOperExpPade = 
+                new OperacionesExpedientePadecimientosFamiliares();
+        return objOperExpPade.listarPadecimientosFamiliares(idExpedientePaciente);
+    }// fin del metodo listarPadecimientosFamiliares
+    
+    public void eliminarPadecimientoFamiliar(String idPadecimiento, String idExpedientePaciente)
+    {
+        OperacionesExpedientePadecimientosFamiliares objOperExpPade = 
+                new OperacionesExpedientePadecimientosFamiliares();
+        objOperExpPade.eliminarPadecimientoFamiliar(idPadecimiento, idExpedientePaciente);
+    }// fin del metodo eliminarPadecimientoFamiliar
+    
 }// fin de la clase NegocioExpedientePadecimientosFamiliares
