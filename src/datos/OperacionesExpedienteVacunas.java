c56868894fe74994dc10dd191749fa6d3bedf57b
QUINCHO <QUINCHO@QUINCHO-THINK>
2016-02-12 18:07:53 -0600

    Agregados los metodo listarVacunas y eliminarVacuna

--- src/datos/OperacionesExpedienteVacunas.java
+++ src/datos/OperacionesExpedienteVacunas.java
@@ -1,6 +1,7 @@
 package datos;
 
 import entidad.ExpedienteVacunas;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -36,4 +37,30 @@ public class OperacionesExpedienteVacunas
         
     }// fin del metodo insertarExpedienteVacunas
     
+    // El suguiente metodo lista las vacunas de un paciente de la base de datos
+    public ResultSet listarVacunas(String idExpedientePaciente) throws SQLException
+    {
+        ResultSet rs;
+        objetoDeConexion = new Conexion();
+        estado = objetoDeConexion.conectar().createStatement();
+        rs = estado.executeQuery("Call ListarVacunas (" + idExpedientePaciente + ")");
+        return rs;
+    }// fin del metodo listarPadecimientosFamiliares
+    
+    // El siguiente metodo elimna vacunas asociadas a un paciente en la BD
+    public void eliminarVacuna(String id)
+    {
+        objetoDeConexion = new Conexion();
+        
+        try
+        {
+            estado = objetoDeConexion.conectar().createStatement();
+            estado.executeQuery("Call EliminarVacunasExpediente(" + id + ")");
+            estado.close();
+        }// fin del try
+        catch(SQLException sqle)
+        {
+            System.out.println(sqle.getErrorCode()+ sqle.getMessage());
+        }// fin del catch
+    }// fin del metodo eliminarDelInventario
 }// fin del metodo OperacionesExpedienteVacunas
