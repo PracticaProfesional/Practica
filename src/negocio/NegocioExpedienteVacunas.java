ab1f77753b23fba1038cd49a2e82e55c6d2f8c4a
QUINCHO <QUINCHO@QUINCHO-THINK>
2016-02-12 18:08:16 -0600

    Agregados los metodo listarVacunas y eliminarVacuna

--- src/negocio/NegocioExpedienteVacunas.java
+++ src/negocio/NegocioExpedienteVacunas.java
@@ -2,6 +2,8 @@ package negocio;
 
 import entidad.ExpedienteVacunas;
 import datos.OperacionesExpedienteVacunas; 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author QUINCHO
@@ -13,5 +15,18 @@ public class NegocioExpedienteVacunas
         OperacionesExpedienteVacunas operacionesExpedienteVacunas = new OperacionesExpedienteVacunas();
         operacionesExpedienteVacunas.insertarExpedienteVacunas(objExpedienteVacunas);
     }// fin del metodo insertarExpedienteVacunas
+    
+    public ResultSet listarVacunas(String idExpedientePaciente) throws SQLException
+    {
+        OperacionesExpedienteVacunas operacionesExpedienteVacunas = new OperacionesExpedienteVacunas();
+        return operacionesExpedienteVacunas.listarVacunas(idExpedientePaciente);
+    }// fin del metodo listarPadecimientosFamiliares
+    
+    public void eliminarVacuna(String id)
+    {
+        OperacionesExpedienteVacunas operacionesExpedienteVacunas = new OperacionesExpedienteVacunas();
+        operacionesExpedienteVacunas.eliminarVacuna(id);
+    }// fin del metodo eliminarVacuna
+    
     
 }// fin de la clase NegocioExpedienteVacunas
