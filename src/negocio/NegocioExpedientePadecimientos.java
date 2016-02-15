115c702e9a87b79946fe723335cf416adb7d1146
QUINCHO <QUINCHO@QUINCHO-THINK>
2016-02-15 00:24:31 -0600

    Agregados los metodos listarPadecimientosPersonales y eliminarPadecimientoPersonal

--- src/negocio/NegocioExpedientePadecimientos.java
+++ src/negocio/NegocioExpedientePadecimientos.java
@@ -3,6 +3,8 @@ package negocio;
 
 import datos.OperacionesExpedientePadecimientos;
 import entidad.ExpedientePadecimientos;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -15,5 +17,19 @@ public class NegocioExpedientePadecimientos
         OperacionesExpedientePadecimientos objOperExpPade = new OperacionesExpedientePadecimientos();
         objOperExpPade.insertarExpedientePadecimientos(objExpedientePadecimientos);
     }// fin del metodo insertarExpedientePadecimientos
+    
+    public ResultSet listarPadecimientosPersonales(String idExpedientePaciente) throws SQLException
+    {
+        OperacionesExpedientePadecimientos objOperExpPade = 
+                new OperacionesExpedientePadecimientos();
+        return objOperExpPade.listarPadecimientosPersonales(idExpedientePaciente);
+    }// fin del metodo listarPadecimientosFamiliares
+    
+    public void eliminarPadecimientoPersonal(String id)
+    {
+        OperacionesExpedientePadecimientos objOperExpPade = 
+                new OperacionesExpedientePadecimientos();
+        objOperExpPade.eliminarPadecimientoPersonal(id);
+    }// fin del metodo eliminarPadecimientoPersonal
     
 }// fin de la clase NegocioExpedientePadecimientos
